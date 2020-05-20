44e40f5fde430aa06eb71a6401ca5579ebea0a4a
Ramil Minyukov <41204824+mrramka@users.noreply.github.com>
2020-05-20 16:08:49 +0300

    Add edge create/delete

--- graph/urls.py
+++ graph/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import url
 from django.urls import path
 
-from graph.views import GraphListView, GraphDetailView
+from graph.views import GraphListView, GraphDetailView, AddNode
 
 app_name = 'graph'
 urlpatterns = [
+    path('add-node/', AddNode.as_view(), name='add_node'),
     path('list/', GraphListView.as_view(), name='to_user_graph_list'),
-    path('<hash>/', GraphDetailView.as_view(), name='to_plan')
+    path('<hash>/', GraphDetailView.as_view(), name='to_plan'),
 ]
--- graph/views.py
+++ graph/views.py
@@ -1,12 +1,14 @@
+import json
 from functools import reduce
 from operator import or_
 
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.db.models import Q
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.views import View
 from django.views.generic import ListView, DetailView
 
-from core.models import Plan
+from core.models import Plan, Color
 from graph.models import Vertex, Edge
 
 
@@ -45,4 +47,29 @@ class GraphDetailView(DetailView, LoginRequiredMixin):
             reduce(or_, [(Q(start_vertex=vertex) | Q(end_vertex=vertex)) for vertex in vertices])
         ).distinct()
         context_data['edges'] = edges
+
+        # Add colors
+        colors = Color.objects.all()
+        context_data['colors'] = colors
         return context_data
+
+
+class AddNode(LoginRequiredMixin, View):
+    def post(self, request):
+        if request.is_ajax():
+            plan_id = request.POST.get('plan_id', None)
+
+            vertex = Vertex.objects.create(
+                name='Node name',
+                color=Color.objects.get(id=1),
+                related_plan=Plan.objects.get(id=plan_id)
+            )
+
+            data = {
+                'v_name': vertex.name,
+                'v_id': vertex.id,
+                'v_color': vertex.color.hex,
+                'v_priority': vertex.priority
+            }
+
+            return HttpResponse(json.dumps(data), content_type='application/json')
